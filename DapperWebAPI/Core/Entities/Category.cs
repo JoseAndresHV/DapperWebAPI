ba6cf52ecabb3b2175d2437d8851ddb132864616
JoseAndresHV <joseandreshurtadov@gmail.com>
2022-12-16 16:22:16 -0400

    Add Category, Product, and Supplier entities.

---
+++ DapperWebAPI/Core/Entities/Category.cs
@@ -0,0 +1,12 @@
+﻿namespace DapperWebAPI.Core.Entities
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; } = null!;
+        public string? Description { get; set; }
+        public byte[]? Picture { get; set; }
+
+        public ICollection<Product>? Products { get; set; }
+    }
+}
---
+++ DapperWebAPI/Core/Entities/Product.cs
@@ -0,0 +1,19 @@
+﻿namespace DapperWebAPI.Core.Entities
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; } = null!;
+        public int? SupplierID { get; set; }
+        public int? CategoryID { get; set; }
+        public int? QuantityPerUnit { get; set; }
+        public decimal? UnitPrice { get; set; }
+        public int? UnitsInStock { get; set; }
+        public int? ReorderLevel { get; set; }
+        public bool Discontinued { get; set; }
+
+        public Supplier? Supplier { get; set; }
+        public Category? Category { get; set; }
+
+    }
+}
