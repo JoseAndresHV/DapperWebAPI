d89df9fb06bb6d1cc73ddbd149d07634e2e0973f
JoseAndresHV <joseandreshurtadov@gmail.com>
2022-12-16 17:09:27 -0400

    Add missing attribute "UnitsOnOrder".

--- DapperWebAPI/Core/Entities/Product.cs
+++ DapperWebAPI/Core/Entities/Product.cs
@@ -9,6 +9,7 @@
         public int? QuantityPerUnit { get; set; }
         public decimal? UnitPrice { get; set; }
         public int? UnitsInStock { get; set; }
+        public int? UnitsOnOrder { get; set; }
         public int? ReorderLevel { get; set; }
         public bool Discontinued { get; set; }
 
