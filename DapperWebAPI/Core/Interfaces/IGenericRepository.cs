4c4cc3f6f0d98fb5a8884632d235f638372c7b92
JoseAndresHV <joseandreshurtadov@gmail.com>
2022-12-16 17:51:26 -0400

    Implement ProductRepository.

--- DapperWebAPI/Core/Interfaces/IGenericRepository.cs
+++ DapperWebAPI/Core/Interfaces/IGenericRepository.cs
@@ -2,7 +2,7 @@
 {
     public interface IGenericRepository<T> where T : class
     {
-        Task<IEnumerable<T>> Get();
+        Task<IEnumerable<T>> GetAll();
         Task<T?> GetById(int id);
         Task<bool> Add(T entity);
         Task<bool> Update(T entity);
---
+++ DapperWebAPI/Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,69 @@
+﻿using Dapper;
+using DapperWebAPI.Core.Entities;
+using DapperWebAPI.Core.Interfaces;
+using Microsoft.Data.SqlClient;
+
+namespace DapperWebAPI.Infrastructure.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionString;
+
+        public ProductRepository(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionString = _configuration.GetConnectionString("DefaultConnection")!;
+        }
+
+        public async Task<bool> Add(Product entity)
+        {
+            var sql = "insert into Products values (@ProductID, @ProductName, @SupplierID, @CategoryID, @QuantityPerUnit, @UnitPrice, @UnitsInStock, @UnitsOnOrder, @ReorderLevel, @Discontinued)";
+            using var connection = new SqlConnection(_connectionString);
+            connection.Open();
+            var result = await connection.ExecuteAsync(sql, entity);
+
+            return result > 0;
+        }
+
+        public async Task<bool> Delete(int id)
+        {
+            var sql = "delete from Products where ProductID = @ProductID";
+            using var connection = new SqlConnection(_connectionString);
+            connection.Open();
+            var result = await connection.ExecuteAsync(sql, new { ProductID = id });
+
+            return result > 0;
+        }
+
+        public async Task<IEnumerable<Product>> GetAll()
+        {
+            var sql = "select * from Products";
+            using var connection = new SqlConnection(_connectionString);
+            connection.Open();
+            var result = await connection.QueryAsync<Product>(sql);
+
+            return result;
+        }
+
+        public async Task<Product?> GetById(int id)
+        {
+            var sql = "select * from Products where ProductID = @ProductID";
+            using var connection = new SqlConnection(_connectionString);
+            connection.Open();
+            var result = await connection.QuerySingleOrDefaultAsync<Product>(sql, new { ProductID = id });
+
+            return result;
+        }
+
+        public async Task<bool> Update(Product entity)
+        {
+            var sql = "update Products set ProductID = @ProductID, ProductName = @ProductName, SupplierID = @SupplierID, CategoryID = @CategoryID, QuantityPerUnit = @QuantityPerUnit, UnitPrice = @UnitPrice, UnitsInStock = @UnitsInStock, UnitsOnOrder = @UnitsOnOrder, ReorderLevel = @ReorderLevel, Discontinued = @Discontinued where ProductID = @ProductID";
+            using var connection = new SqlConnection(_connectionString);
+            connection.Open();
+            var result = await connection.ExecuteAsync(sql, entity);
+
+            return result > 0;
+        }
+    }
+}
