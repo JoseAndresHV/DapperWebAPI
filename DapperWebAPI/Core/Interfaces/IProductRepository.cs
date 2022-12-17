b2f5ee865de4bda5ba3c67d2169693f46abe4cab
JoseAndresHV <joseandreshurtadov@gmail.com>
2022-12-17 02:49:49 -0400

    Add detailed methods.

--- DapperWebAPI/Core/Interfaces/IProductRepository.cs
+++ DapperWebAPI/Core/Interfaces/IProductRepository.cs
@@ -4,5 +4,7 @@ namespace DapperWebAPI.Core.Interfaces
 {
     public interface IProductRepository : IGenericRepository<Product>
     {
+        Task<IEnumerable<Product>> GetAllDetailed();
+        Task<Product?> GetByIdDetailed(int id);
     }
 }
--- DapperWebAPI/Infrastructure/Repositories/ProductRepository.cs
+++ DapperWebAPI/Infrastructure/Repositories/ProductRepository.cs
@@ -18,7 +18,10 @@ namespace DapperWebAPI.Infrastructure.Repositories
 
         public async Task<bool> Add(Product entity)
         {
-            var sql = "insert into Products values (@ProductID, @ProductName, @SupplierID, @CategoryID, @QuantityPerUnit, @UnitPrice, @UnitsInStock, @UnitsOnOrder, @ReorderLevel, @Discontinued)";
+            var sql = @"insert into Products (ProductName, SupplierID, CategoryID, QuantityPerUnit, 
+                        UnitPrice, UnitsInStock, UnitsOnOrder, ReorderLevel, Discontinued)
+                        values (@ProductName, @SupplierID, @CategoryID, @QuantityPerUnit, 
+                        @UnitPrice, @UnitsInStock, @UnitsOnOrder, @ReorderLevel, @Discontinued)";
             using var connection = new SqlConnection(_connectionString);
             connection.Open();
             var result = await connection.ExecuteAsync(sql, entity);
@@ -28,7 +31,8 @@ namespace DapperWebAPI.Infrastructure.Repositories
 
         public async Task<bool> Delete(int id)
         {
-            var sql = "delete from Products where ProductID = @ProductID";
+            var sql = @"delete from Products 
+                        where ProductID = @ProductID";
             using var connection = new SqlConnection(_connectionString);
             connection.Open();
             var result = await connection.ExecuteAsync(sql, new { ProductID = id });
@@ -38,7 +42,7 @@ namespace DapperWebAPI.Infrastructure.Repositories
 
         public async Task<IEnumerable<Product>> GetAll()
         {
-            var sql = "select * from Products";
+            var sql = @"select * from Products";
             using var connection = new SqlConnection(_connectionString);
             connection.Open();
             var result = await connection.QueryAsync<Product>(sql);
@@ -46,9 +50,30 @@ namespace DapperWebAPI.Infrastructure.Repositories
             return result;
         }
 
+        public async Task<IEnumerable<Product>> GetAllDetailed()
+        {
+            var sql = @"select p.*, s.*, c.CategoryID, c.CategoryName from Products p
+                        left join Suppliers s
+                        on p.SupplierID = s.SupplierID
+                        left join Categories c
+                        on p.CategoryID = c.CategoryID";
+            using var connection = new SqlConnection(_connectionString);
+            connection.Open();
+            var result = await connection.QueryAsync<Product, Supplier, Category, Product>(sql,
+                (product, supplier, category) =>
+                {
+                    product.Supplier = supplier;
+                    product.Category = category;
+                    return product;
+                }, splitOn: "SupplierId,CategoryId");
+
+            return result;
+        }
+
         public async Task<Product?> GetById(int id)
         {
-            var sql = "select * from Products where ProductID = @ProductID";
+            var sql = @"select * from Products 
+                        where ProductID = @ProductID";
             using var connection = new SqlConnection(_connectionString);
             connection.Open();
             var result = await connection.QuerySingleOrDefaultAsync<Product>(sql, new { ProductID = id });
@@ -56,9 +81,38 @@ namespace DapperWebAPI.Infrastructure.Repositories
             return result;
         }
 
+        public async Task<Product?> GetByIdDetailed(int id)
+        {
+            var sql = @"select p.*, s.*, c.CategoryID, c.CategoryName from Products p
+                        left join Suppliers s
+                        on p.SupplierID = s.SupplierID
+                        left join Categories c
+                        on p.CategoryID = c.CategoryID
+                        where ProductID = @ProductID";
+            using var connection = new SqlConnection(_connectionString);
+            connection.Open();
+            var result = await connection.QueryAsync<Product, Supplier, Category, Product>(
+                sql,
+                (product, supplier, category) =>
+                {
+                    product.Supplier = supplier;
+                    product.Category = category;
+                    return product;
+                },
+                param: new { ProductID = id },
+                splitOn: "SupplierId,CategoryId");
+
+            return result.FirstOrDefault();
+        }
+
         public async Task<bool> Update(Product entity)
         {
-            var sql = "update Products set ProductID = @ProductID, ProductName = @ProductName, SupplierID = @SupplierID, CategoryID = @CategoryID, QuantityPerUnit = @QuantityPerUnit, UnitPrice = @UnitPrice, UnitsInStock = @UnitsInStock, UnitsOnOrder = @UnitsOnOrder, ReorderLevel = @ReorderLevel, Discontinued = @Discontinued where ProductID = @ProductID";
+            var sql = @"update Products set ProductName = @ProductName, 
+                        SupplierID = @SupplierID, CategoryID = @CategoryID, 
+                        QuantityPerUnit = @QuantityPerUnit, UnitPrice = @UnitPrice, 
+                        UnitsInStock = @UnitsInStock, UnitsOnOrder = @UnitsOnOrder, 
+                        ReorderLevel = @ReorderLevel, Discontinued = @Discontinued 
+                        where ProductID = @ProductID";
             using var connection = new SqlConnection(_connectionString);
             connection.Open();
             var result = await connection.ExecuteAsync(sql, entity);
